0259266f41ce99fdc6e9a25f86a43c6e577d96f6
freshloic <loicjeanchrles@gmail.com>
2018-09-25 22:12:59 +0200

    Refactorisation du nom du virtual environnement en venv

--- django-project/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/freshloic/Documents/UA/IngenierieLogiciel/django-project/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
